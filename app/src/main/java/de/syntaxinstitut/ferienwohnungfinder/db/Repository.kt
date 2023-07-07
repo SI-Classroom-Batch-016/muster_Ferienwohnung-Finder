c79b7010800141c481a3ea949fad69816c47cf7b
FabianAMoreno <fabian.moreno@syntax-institut.de>
2023-07-07 11:36:49 +0200

    small changes

--- app/src/main/java/de/syntaxinstitut/ferienwohnungfinder/db/Repository.kt
+++ app/src/main/java/de/syntaxinstitut/ferienwohnungfinder/db/Repository.kt
@@ -12,7 +12,6 @@ class Repository(private val database: AppartmentDatabase) {
      *
      * @return Alle Items der Tabelle
      */
-    // todo: getAllItems(), schreibe hier eine Funktion, welche alle Datensätze zurück liefert
     fun getAllItems(): List<AppartmentData> {
         return database.appartmentDao.getItems()
     }
@@ -23,7 +22,6 @@ class Repository(private val database: AppartmentDatabase) {
      *
      * @return Anzahl der Items der Tabelle
      */
-    // todo: getCount(), schreibe hier eine Funktion, welche die Anzahl der Datensätze zurück liefert
     fun getCount(): Int {
         return database.appartmentDao.getCount()
     }
@@ -33,9 +31,6 @@ class Repository(private val database: AppartmentDatabase) {
      * Füllt die Datenbank mit Beispielen, wenn die Datenbank leer ist.
      */
     fun prepopulateDB() {
-        // todo: Schreibe eine Funktion prepopulateDB() welche die Datenbank mit den
-        //  Beispieldatensätzen befüllt, wenn diese leer ist
-
         try {
             // Add example appartments to database
             if (database.appartmentDao.getCount() == 0) {
